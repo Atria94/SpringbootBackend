0b0889dad6ce005db3fdf6c8f082a7e842b10a58
Atria94 <atriaclase@gmail.com>
2022-04-04 12:03:18 +0200

    aaa

--- src/main/java/com/example/demo/controllers/Ejercicio.java
+++ src/main/java/com/example/demo/controllers/Ejercicio.java
@@ -1,9 +1,12 @@
 package com.example.demo.controllers;
 
+import java.text.MessageFormat;
+
 import com.example.demo.utils.Utils;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,4 +30,15 @@ public class Ejercicio {
         boolean palindrome= Utils.isPalindrome(name);
         return palindrome ? "Si es palindromo" : "No es palindromo";
     }
+
+    //http://localhost:8080/sumar?num1=5&num2=2
+    @GetMapping("/sumar")
+    public String add(@RequestParam String num1,
+                        @RequestParam String num2){
+        int resultado = Integer.parseInt(num1) + Integer.parseInt(num2);
+        //return "La suma de "+num1+" y el numero "+num2+" es "+resultado;
+        Object params[] = {num1, num2, resultado};
+        return MessageFormat.format("La suma de {0} y {1} es {2}")
+    }
+
 }
